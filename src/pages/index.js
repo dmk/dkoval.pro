83820029d1a76227cbf5462d2938d58831080476
Dmytro Koval <contact@dkoval.pro>
2024-07-27 11:50:10 +0300

    Add links to my social networks

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,14 @@ import { Raleway, Itim } from "next/font/google";
 
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
+import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import { styled } from '@mui/system';
+
+import GitHub from "@mui/icons-material/GitHub";
+import LinkedIn from '@mui/icons-material/LinkedIn';
+import Twitter from '@mui/icons-material/Twitter';
+import X from '@mui/icons-material/X';
 
 const raleway = Raleway({ subsets: ["latin"] });
 const handwriten = Itim({ subsets: ["latin"], weight: ["400"] });
@@ -30,7 +36,7 @@ const Country = styled('span')(({ theme }) => ({
 export default function Home() {
   return (
     <Container maxWidth="sm">
-      <Box sx={{ my: 12 }}>
+      <Box sx={{ mt: 12 }}>
         <Text variant="h4" component="h1" gutterBottom>
           Hi, I&apos;m <Name>Dima</Name>, a DevOps Engineer from <City>Kyiv</City>, <Country>Ukraine</Country>.
         </Text>
@@ -38,6 +44,18 @@ export default function Home() {
           I work in gamedev, passionate about automation, and enjoy doing web development for fun.
         </Text>
       </Box>
+
+      <Box sx={{ display: 'flex', justifyContent: 'center', mt: 2 }}>
+          <IconButton href="https://github.com/dmk" target="_blank">
+            <GitHub />
+          </IconButton>
+          <IconButton href="https://linkedin.com/in/dmk" target="_blank">
+            <LinkedIn />
+          </IconButton>
+          <IconButton href="https://twitter.com/dmkov41" target="_blank">
+            <Twitter />
+          </IconButton>
+        </Box>
     </Container>
   );
 }

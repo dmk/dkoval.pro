a6b78253000d4d89da8ce5683c7b64453b58bb8e
Dmytro Koval <contact@dkoval.pro>
2024-07-29 20:57:12 +0300

    Tune the page transition

--- src/components/PageTransition.js
+++ src/components/PageTransition.js
@@ -3,14 +3,14 @@ import { motion, AnimatePresence } from 'framer-motion';
 const variants = {
   hidden: (direction) => ({
     opacity: 0,
-    x: direction === 'right' ? -200 : direction === 'left' ? 200 : 0,
-    y: direction === 'down' ? -200 : direction === 'up' ? 200 : 0,
+    x: direction === 'right' ? -100 : direction === 'left' ? 100 : 0,
+    y: direction === 'down' ? -100 : direction === 'up' ? 100 : 0,
   }),
   enter: { opacity: 1, x: 0, y: 0 },
   exit: (direction) => ({
     opacity: 0,
-    x: direction === 'right' ? 200 : direction === 'left' ? -200 : 0,
-    y: direction === 'down' ? 200 : direction === 'up' ? -200 : 0,
+    x: direction === 'right' ? 100 : direction === 'left' ? -100 : 0,
+    y: direction === 'down' ? 100 : direction === 'up' ? -100 : 0,
   }),
 };
 
@@ -23,7 +23,7 @@ const PageTransition = ({ children, direction }) => {
         animate="enter"
         exit="exit"
         variants={variants}
-        transition={{ type: 'linear' }}
+        transition={{ type: 'easeIn', duration: .4 }}
         style={{ position: 'absolute', width: '100%' }}
       >
         {children}

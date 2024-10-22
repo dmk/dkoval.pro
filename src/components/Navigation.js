afa77604a06dec9ac66e418ba663d370e0c587fb
Dmytro Koval <contact@dkoval.pro>
2024-10-22 21:36:40 +0300

    Add nav tooltips

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -31,37 +31,44 @@ const navItems = [
 
 const Navigation = () => {
   const router = useRouter();
-  const [value, setValue] = useState(router.pathname);
-
-  useEffect(() => {
-    setValue(router.pathname);
-  }, [router.pathname]);
 
   return (
     <nav
       className="
-        fixed 
-        bg-white 
-        rounded-full 
-        shadow-md 
-        p-3 
-        flex 
-        space-x-4 
+        fixed rounded-full
+        p-2 flex space-x-4
         transition-all
         bottom-4 left-1/2 transform -translate-x-1/2
-        md:bottom-auto md:left-4 md:top-32 md:-translate-y-1/3 md:translate-x-0 md:flex-col md:space-x-0 md:space-y-3
+        bg-white shadow-lg
+
+        md:bottom-auto md:left-4 md:top-32 md:-translate-y-1/3 md:translate-x-0
+        md:flex-col md:space-x-0 md:space-y-3 md:shadow-none md:bg-none
       "
     >
       {navItems.map(item => (
-        <button
-          key={item.label}
-          onClick={() => router.push(item.href)}
-          className={`flex items-center justify-center p-3 rounded-full transition-all w-12 h-12 
-                      ${item.href === value ? 'bg-green-600 text-white' : 'bg-gray-200 text-gray-700'}
-                      hover:bg-green-500 hover:text-white shadow-sm duration-300`}
-        >
-          {item.icon}
-        </button>
+        <div key={item.label} className="relative group">
+          <a
+            onClick={() => router.push(item.href)}
+            className={`flex items-center justify-center p-3 rounded-full transition-all w-12 h-12 
+                        ${item.href === router.pathname ? 'bg-green-600 text-white' : 'bg-gray-200 text-gray-700'}
+                        hover:bg-green-500 hover:text-white shadow-sm duration-300
+                        cursor-pointer`}
+            id={item.tooltipId}
+            data-tooltip-target={`#nav-item-${item.label}`}
+          >
+            {item.icon}
+          </a>
+          <div
+            className="absolute w-max px-2 py-1 bg-gray-800 text-white duration-200
+                       text-xs rounded opacity-0 group-hover:opacity-100 transition-opacity
+                       bottom-full left-1/2 -translate-x-1/2 mb-3
+                       md:top-1/4 md:left-full md:ml-3 md:bottom-0 md:bottom-0 md:translate-x-0"
+            role="tooltip"
+            id={`nav-item-${item.label}`}
+          >
+            {item.label}
+          </div>
+        </div>
       ))}
     </nav>
   );
